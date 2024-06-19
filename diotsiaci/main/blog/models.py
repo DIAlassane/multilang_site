83d4b9bc764999ff35749fa2cbfb456886c51393
DIAlassane <alassdia0909@gmail.com>
2024-06-19 22:57:46 +0200

    Front 1
    Mise en place du côté admin :
    -post : title, content et publication_date
    -commencement du home

--- diotsiaci/main/blog/models.py
+++ diotsiaci/main/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=500)
+    publication_date = models.DateTimeField(auto_now_add=True)
+
+class Meta:
+    ordering = ['-date_added']
