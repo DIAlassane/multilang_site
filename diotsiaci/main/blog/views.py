ab7e221df5eb9324aad8c5319ddb09cb4745eb52
DIAlassane <alassdia0909@gmail.com>
2024-06-24 23:34:52 +0200

    Commit 3
    Fin du GPT debut du RAG

--- diotsiaci/main/blog/views.py
+++ diotsiaci/main/blog/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import Post, Document
 import openai
 
-api_key = 'sk-proj-RVdItmDxN14DhXGVOgifT3BlbkFJ8CxeucIFBYTABhRm9lee'
+api_key = 'sk-proj-TthpmGB9V2JyshHARmH4T3BlbkFJpndemZAZM0ALVDcO4ErS'
 
 def switch_to_french(request):
     activate('fr')
@@ -22,18 +22,21 @@ def ExtendedFront(request):
     if request.method == 'POST':
         openai.api_key = api_key
         user_input = request.POST.get('user_input')
-        prompt = user_input
-
-        response = openai.Completion.create(
-            engine="gpt-3.5-turbo-instruct",
-            prompt=prompt,
+        
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a master poet. Craft beautiful, emotive, and lyrical poems on any topic."},
+                {"role": "user", "content": user_input}
+            ],
             max_tokens=150,
             temperature=0.5
         )
 
-        response_text = response['choices'][0]['text'].strip()
+        response_text = response['choices'][0]['message']['content'].strip()
 
     return render(request, 'blog/ExtendedFront.html', {'posts': posts, 'response': response_text})
+
 def search_documents(query):
     documents = Document.objects.all()
     results = []
