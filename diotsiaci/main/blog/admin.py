2f81050cd2715ebc627225ba35861b08842a7ca4
DIAlassane <alassdia0909@gmail.com>
2024-06-24 19:59:23 +0200

    commit 2
    GPT en cours + CSS avec bulma

--- diotsiaci/main/blog/admin.py
+++ diotsiaci/main/blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from .models import Post, Document, Ressource
 
-from .models import Post
-
+# Enregistrer chaque modèle dans le site d'administration
 admin.site.register(Post)
+admin.site.register(Document)
+admin.site.register(Ressource)
--- diotsiaci/main/blog/views.py
+++ diotsiaci/main/blog/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-from .models import Post
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 from django.utils.translation import activate
+from django.views.decorators.csrf import csrf_exempt
+from .models import Post, Document
+import openai
+
+api_key = 'sk-proj-RVdItmDxN14DhXGVOgifT3BlbkFJ8CxeucIFBYTABhRm9lee'
 
 def switch_to_french(request):
     activate('fr')
@@ -11,7 +14,30 @@ def switch_to_english(request):
     activate('en')
     return redirect('/')
 
-
+@csrf_exempt
 def ExtendedFront(request):
     posts = Post.objects.all()
-    return render(request, 'blog/ExtendedFront.html', {'posts': posts})
+    response_text = None
+
+    if request.method == 'POST':
+        openai.api_key = api_key
+        user_input = request.POST.get('user_input')
+        prompt = user_input
+
+        response = openai.Completion.create(
+            engine="gpt-3.5-turbo-instruct",
+            prompt=prompt,
+            max_tokens=150,
+            temperature=0.5
+        )
+
+        response_text = response['choices'][0]['text'].strip()
+
+    return render(request, 'blog/ExtendedFront.html', {'posts': posts, 'response': response_text})
+def search_documents(query):
+    documents = Document.objects.all()
+    results = []
+    for doc in documents:
+        if query.lower() in doc.content.lower():
+            results.append(doc)
+    return results
